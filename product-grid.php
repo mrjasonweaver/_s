af78ac6a265cb567004f5b072fda3e764f420250
mrjasonweaver <indyplanets@gmail.com>
2021-01-06 17:25:50 -0600

    add styles for product grid

---
+++ product-grid.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Product Grid
+ *
+ * @package _s
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+        <h1>Product Grid</h1>
+
+        <div class="product-grid">
+            <div>
+                <img src="<?php echo get_template_directory_uri() . '/yum.png' ?>" />
+                <div class="price">10</div>
+            </div>
+            <div>
+                <img src="<?php echo get_template_directory_uri() . '/yum.png' ?>" />
+                <div class="price">20</div>
+            </div>
+            <div>
+                <img src="<?php echo get_template_directory_uri() . '/yum.png' ?>" />
+                <div class="price">30</div>
+            </div>
+            <div>
+                <img src="<?php echo get_template_directory_uri() . '/yum.png' ?>" />
+                <div class="price">40</div>
+            </div>
+        </div>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
